17c4850d34b22341d83c7e586dd3af08587959b1
Jakub Kozłowski <kubukoz@gmail.com>
2019-01-16 18:47:29 +0100

    Rename instance

--- src/main/scala/slickeffect/implicits.scala
+++ src/main/scala/slickeffect/implicits.scala
@@ -8,5 +8,5 @@ import scala.concurrent.ExecutionContext
 object implicits extends DBIOInstances
 
 trait DBIOInstances {
-  implicit def dbioASync(implicit ec: ExecutionContext): Async[DBIO] = new DBIOAsync()
+  implicit def dbioCatsEffectAsync(implicit ec: ExecutionContext): Async[DBIO] = new DBIOAsync()
 }
