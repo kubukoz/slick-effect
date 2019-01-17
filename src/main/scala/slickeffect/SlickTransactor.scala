f450aeff18d1a5f2e6f21b61ded35a8d051b4741
Jakub Kozłowski <kubukoz@gmail.com>
2019-01-17 19:50:56 +0100

    Make `transactK` the abstract one

--- src/main/scala/slickeffect/SlickTransactor.scala
+++ src/main/scala/slickeffect/SlickTransactor.scala
@@ -2,17 +2,17 @@ package slickeffect
 
 import cats.effect._
 import cats.effect.implicits._
-import cats.implicits._
 import cats.~>
 import slick.basic.{BasicProfile, DatabaseConfig}
 import slick.dbio.DBIO
 import slick.jdbc.JdbcProfile
+import cats.implicits._
 
 trait SlickTransactor[F[_]] {
-  def transact[A](dbio: DBIO[A]): F[A]
-  def transactK: DBIO ~> F = λ[DBIO ~> F](transact(_))
+  def transact[A](dbio: DBIO[A]): F[A] = transactK(dbio)
+  def transactK: DBIO ~> F
 
-  def configure(f: DBIO ~> DBIO): SlickTransactor[F] = new ConfiguredSlickTransactor(this, f)
+  def configure(f: DBIO ~> DBIO): SlickTransactor[F] = SlickTransactor.liftNT(f andThen transactK)
 }
 
 object SlickTransactor {
@@ -20,9 +20,10 @@ object SlickTransactor {
   def fromDatabase[F[_]: Async: ContextShift](
     dbF: F[BasicProfile#Backend#DatabaseDef]): Resource[F, SlickTransactor[F]] =
     Resource.make(dbF)(db => IO.fromFuture(IO(db.shutdown)).to[F]).map { db =>
-      new SlickTransactor[F] {
-        override def transact[A](dbio: DBIO[A]): F[A] =
+      liftNT {
+        λ[DBIO ~> F] { dbio =>
           IO.fromFuture(IO(db.run(dbio))).to[F].guarantee(ContextShift[F].shift)
+        }
       }
     }
 
@@ -30,12 +31,14 @@ object SlickTransactor {
     dbConfig: DatabaseConfig[_ <: BasicProfile]): Resource[F, SlickTransactor[F]] =
     fromDatabase(Sync[F].delay(dbConfig.db))
 
+  def liftNT[F[_]](f: DBIO ~> F): SlickTransactor[F] = new SlickTransactor[F] { override val transactK: DBIO ~> F = f }
+
+  //////////////
+  //CONFIGURATION
+  //////////////
+
   def transactionally(implicit profile: JdbcProfile): DBIO ~> DBIO = {
     import profile.api.{DBIO => _, _}
     λ[DBIO ~> DBIO](_.transactionally)
   }
-}
-
-class ConfiguredSlickTransactor[F[_]](underlying: SlickTransactor[F], f: DBIO ~> DBIO) extends SlickTransactor[F] {
-  override def transact[A](dbio: DBIO[A]): F[A] = underlying.transact(f(dbio))
 }
